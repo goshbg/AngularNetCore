7343507c04a8b0f1e21d000b70459f8278bff291
Georgi Takov <georgi.takov@endava.com>
2018-05-04 13:59:36 +0300

    Refactor IHeroesRepository interface

--- AngularNetCore.Repository/Interfaces/IHeroesRepository.cs
+++ AngularNetCore.Repository/Interfaces/IHeroesRepository.cs
@@ -6,7 +6,7 @@ namespace AngularNetCore.Repository.Interfaces
     public interface IHeroesRepository
     {
         HeroDto GetHeroById(int id);
-        IList<HeroDto> GetHeroes(string searchString, int startIndex, int limit);
+        IList<HeroDto> GetHeroes(string searchString, int currentPage, int pageSize);
         int GetHeroesCount(string searchString);
     }
 }
