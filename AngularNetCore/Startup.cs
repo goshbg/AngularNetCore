5199b6e3cd3745938b28b56491d9e0ada3403bfc
Georgi Takov <georgi.takov@endava.com>
2018-05-04 11:10:51 +0300

    Add reference to Repository in the Web App. Configure connection string in appsettings.json

--- AngularNetCore/Startup.cs
+++ AngularNetCore/Startup.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AngularNetCore.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
@@ -23,6 +20,7 @@ namespace AngularNetCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            RepositoryConfiguraiton.ConfigureServices(services, Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
