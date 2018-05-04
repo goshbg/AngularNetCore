c615760dd69a4c88854e9b7ee4f19b39620c2d34
Georgi Takov <georgi.takov@endava.com>
2018-05-04 10:58:31 +0300

    Add Data Access project

---
+++ AngularNetCore.DataAccess/Models/AngularnetcoreContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularNetCore.DataAccess.Models
+{
+    public partial class AngularnetcoreContext : DbContext
+    {
+        public virtual DbSet<Hero> Heroes { get; set; }
+
+        public AngularnetcoreContext(DbContextOptions<AngularnetcoreContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Hero>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+        }
+    }
+}
---
+++ AngularNetCore.DataAccess/Models/Hero.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AngularNetCore.DataAccess.Models
+{
+    public partial class Hero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
