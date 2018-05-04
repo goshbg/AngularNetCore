35b308aca29a749dd46deb9c6c90912c113ffce8
Georgi Takov <georgi.takov@endava.com>
2018-05-04 11:10:00 +0300

    Add Repository project

---
+++ AngularNetCore.Repository/RepositoryConfiguraiton.cs
@@ -0,0 +1,16 @@
+﻿using AngularNetCore.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AngularNetCore.Repository
+{
+    public static class RepositoryConfiguraiton
+    {
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AngularnetcoreContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("AngularNetCore")));
+        }
+    }
+}
